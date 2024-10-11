86f92c4968afec49209cc315dac51b3d44a9c1d3
Apple <apple@Apples-MacBook-Pro.local>
2024-10-11 18:45:09 +0100

    first commit the wild oasis app

--- app/_lib/supabase.ts
+++ app/_lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_KEY as string
+  process.env.SUPABASE_URL || "https://ozmavzxnpughpcrqwsfa.supabase.co",
+  process.env.SUPABASE_KEY ||
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96bWF2enhucHVnaHBjcnF3c2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg1NTg2MzMsImV4cCI6MjA0NDEzNDYzM30.BO-Jxw2vHWm3qXn9C8CFqaveaeR_qK1FsytRdWS3Tms"
 );
 
 export default supabase;
